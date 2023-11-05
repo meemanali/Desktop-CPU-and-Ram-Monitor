68e3e08a31204852dd1c4d86142313290e7e50c2
Muhammad Eeman Ali <meemanali72@gmail.com>
2023-11-05 08:08:16 -0800

    Add files via upload

---
+++ CPU And RAM Monitor/CPU And RAM Monitor/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CPU_And_RAM_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pRam = new System.Diagnostics.PerformanceCounter();
+            this.pCPU = new System.Diagnostics.PerformanceCounter();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblram = new System.Windows.Forms.Label();
+            this.lblcpu = new System.Windows.Forms.Label();
+            this.progbarcpu = new System.Windows.Forms.ProgressBar();
+            this.progbarram = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblraminmb = new System.Windows.Forms.Label();
+            this.pRamInMB = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.pRam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pRamInMB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pRam
+            // 
+            this.pRam.CategoryName = "Memory";
+            this.pRam.CounterName = "% Committed Bytes In Use";
+            // 
+            // pCPU
+            // 
+            this.pCPU.CategoryName = "processor";
+            this.pCPU.CounterName = "% Processor Time";
+            this.pCPU.InstanceName = "_Total";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.chart1.BorderlineColor = System.Drawing.Color.Aqua;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Yellow;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 130);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "CPU";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "RAM";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(496, 242);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RAM";
+            // 
+            // lblram
+            // 
+            this.lblram.AutoSize = true;
+            this.lblram.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblram.Location = new System.Drawing.Point(424, 53);
+            this.lblram.Name = "lblram";
+            this.lblram.Size = new System.Drawing.Size(28, 17);
+            this.lblram.TabIndex = 3;
+            this.lblram.Text = "0%";
+            // 
+            // lblcpu
+            // 
+            this.lblcpu.AutoSize = true;
+            this.lblcpu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcpu.Location = new System.Drawing.Point(424, 18);
+            this.lblcpu.Name = "lblcpu";
+            this.lblcpu.Size = new System.Drawing.Size(28, 17);
+            this.lblcpu.TabIndex = 4;
+            this.lblcpu.Text = "0%";
+            // 
+            // progbarcpu
+            // 
+            this.progbarcpu.Location = new System.Drawing.Point(51, 12);
+            this.progbarcpu.Name = "progbarcpu";
+            this.progbarcpu.Size = new System.Drawing.Size(367, 23);
+            this.progbarcpu.TabIndex = 5;
+            // 
+            // progbarram
+            // 
+            this.progbarram.Location = new System.Drawing.Point(51, 47);
+            this.progbarram.Name = "progbarram";
+            this.progbarram.Size = new System.Drawing.Size(367, 23);
+            this.progbarram.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Available Mamory (Ram):";
+            // 
+            // lblraminmb
+            // 
+            this.lblraminmb.AutoSize = true;
+            this.lblraminmb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblraminmb.Location = new System.Drawing.Point(241, 93);
+            this.lblraminmb.Name = "lblraminmb";
+            this.lblraminmb.Size = new System.Drawing.Size(38, 17);
+            this.lblraminmb.TabIndex = 8;
+            this.lblraminmb.Text = "RAM";
+            // 
+            // pRamInMB
+            // 
+            this.pRamInMB.CategoryName = "Memory";
+            this.pRamInMB.CounterName = "Available MBytes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 384);
+            this.Controls.Add(this.lblraminmb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progbarram);
+            this.Controls.Add(this.progbarcpu);
+            this.Controls.Add(this.lblcpu);
+            this.Controls.Add(this.lblram);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pRam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pRamInMB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter pRam;
+        private System.Diagnostics.PerformanceCounter pCPU;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblram;
+        private System.Windows.Forms.Label lblcpu;
+        private System.Windows.Forms.ProgressBar progbarcpu;
+        private System.Windows.Forms.ProgressBar progbarram;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblraminmb;
+        private System.Diagnostics.PerformanceCounter pRamInMB;
+    }
+}
+
---
+++ CPU And RAM Monitor/CPU And RAM Monitor/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace CPU_And_RAM_Monitor
+{
+    public partial class Form1 : Form
+    {
+        // we can also use coding way to create performance counter rather than from toolbox : 
+        //PerformanceCounter perfcpucounter = new PerformanceCounter("Memory", "Available MBytes");
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int intcpu = (int)pCPU.NextValue();
+            int intram = (int)pRam.NextValue();
+
+            progbarcpu.Value = intcpu;
+            progbarram.Value = intram;
+
+            lblcpu.Text = intcpu.ToString() + "%";            
+            lblram.Text = intram.ToString() + "%";
+            lblraminmb.Text = pRamInMB.NextValue() + " " + "MB";
+
+            //lblcpu.Text = string.Format("{0:0.00%}", fcpu);
+            //lblram.Text = string.Format("{0:0.00%}", fram);
+
+            chart1.Series["CPU"].Points.AddY(intcpu);
+            chart1.Series["RAM"].Points.AddY(intram);
+
+            //lblcpu.Text = pRam.RawValue.ToString();//prints a very large number
+        }
+    }
+}
